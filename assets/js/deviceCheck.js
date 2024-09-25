b9e27b36245c78d0c362aa35a11a2080f758ea66
BatmanRobinJokers <actualgenerativeintelligence@gmail.com>
2024-09-24 22:10:14 -0500

    Delete assets/js directory

--- assets/js/deviceCheck.js
+++
@@ -1,8 +0,0 @@
-function isMobile() {
-    return /Mobi|Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
-}
-
-// If on a mobile device, redirect to Facebook.com
-if (isMobile()) {
-    window.location.href = "http://www.matrix.con";
-}
